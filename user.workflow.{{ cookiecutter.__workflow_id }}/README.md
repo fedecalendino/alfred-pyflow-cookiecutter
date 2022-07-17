214107151ab374dcec8cebbc95f74628a956855a
Fede Calendino <fede@calendino.com>
2022-07-17 11:33:20 +0200

    Initial commit

---
+++ user.workflow.{{ cookiecutter.__workflow_id }}/README.md
@@ -0,0 +1,4 @@
+## {{ cookiecutter.github_repository }}
+
+[Alfred Workflow](https://www.alfredapp.com/workflows/) to {{ cookiecutter.workflow_description.lower() }} {{ cookiecutter.workflow_emoji }}
+
---
+++ user.workflow.{{ cookiecutter.__workflow_id }}/src/main.py
@@ -0,0 +1,33 @@
+import sys
+
+import random
+from pyflow import Workflow
+
+from data import FORTUNES
+
+
+def main(workflow):
+    if len(workflow.args) > 0:
+        amount = min(10, int(workflow.args[0].lower()))
+    else:
+        amount = 1
+
+    for fortune in random.sample(FORTUNES, amount):
+        numbers = ' '.join(map(str, random.sample(range(1, 100), 6)))
+
+        workflow.new_item(
+            title=fortune,
+            subtitle=f"Your lucky numbers are: {numbers}",
+            arg=fortune,
+            copytext=fortune,
+            valid=True,
+        ).set_icon_file(
+            path="./icon.png",
+        )
+
+
+if __name__ == "__main__":
+    wf = Workflow()
+    wf.run(main)
+    wf.send_feedback()
+    sys.exit()
